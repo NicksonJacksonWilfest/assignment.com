7b33c8fefa87af5b8feff875d8509e537f6738bc
NicksonJacksonWilfest <devxjr.pro@gmail.com>
2024-04-09 21:11:02 +0300

    assignment for 21 days

---
+++ clean_one/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Portfolio</li>
+            <li>Services</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  );
+}
+export default Navbar;
---
+++ clean_one/src/layout/Contact.jsx
@@ -0,0 +1,9 @@
+function Contact() {
+    return (
+        <div>
+            <h1 className="text-4xl text-center text-blue-700">Contact</h1>
+            <p className="text-center text-xl">Contact us on: 0712345678</p>
+        </div>
+    )
+}
+export default Contact

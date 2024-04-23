a4ac0bc467d8354dd6df0cfff31883216b5923b2
Distrodev <137993578+NicksonJacksonWilfest@users.noreply.github.com>
2024-04-23 03:30:09 -0700

    Delete clean_one/src directory

--- clean_one/src/layout/Home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-    return (
-        <div>
-            <h1 className="text-red-700 text-xl">Home</h1>
-        </div>
-    )
-}
-
-export default Home
--- clean_one/src/layout/Portfolio.jsx
+++
@@ -1,9 +0,0 @@
-function Portfolio() {
-    return (
-        <div>
-            <h1 className="text-4xl text-center font-bold">Portfolio</h1>
-            <p className="text-center text-lg">Welcome to the Portfolio page</p>
-        </div>
-    )
-}
-export default Portfolio
